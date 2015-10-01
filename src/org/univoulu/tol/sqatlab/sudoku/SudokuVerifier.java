6bae08654995a74d0b1e3d410971e6bf86f6427a
somename <someemail>
2015-10-01 13:38:26 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -23,9 +23,9 @@ public class SudokuVerifier {
 	public int rule3(String sudoku) {
 		String partString;
 		for( int i = 1; i < 10; i++ ) {
-			partString = sudoku.substring(0+i*9,9+i*9);
+	//		partString = sudoku.substring(0+i*9,9+i*9);
 
-	//	partString = sudoku.substring(0,9);
+		partString = sudoku.substring(0,9);
 			for( int n = 1; n < 10; n++ ) {
 				if (!partString.contains(String.valueOf(n))) {
 					return -3;
