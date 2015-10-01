d6b3b51fc7dede751073c23d891be0dcb04abda6
somename <someemail>
2015-10-01 12:43:19 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -8,6 +8,7 @@ public class TestSudokuVerifier {
 	String correctString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	String incorrectString ="123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	String tooLongString ="1234567899123456788912345677891234566789123455678912344567891233456789122345678914587";
+	String notDigitsString = "A417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	
 	SudokuVerifier sudoku = new SudokuVerifier();
 
@@ -31,7 +32,7 @@ public class TestSudokuVerifier {
 	
 	@Test
 	public void testRule1ResultsMinus1() {
-		int test = sudoku.rule1(incorrectString);
+		int test = sudoku.rule1(notDigitsString);
 		assertEquals(test, test=-1);
 	}
 
