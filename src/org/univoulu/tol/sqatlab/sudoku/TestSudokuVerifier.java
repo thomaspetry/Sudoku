4b7af5453567fcbaf721750a43a82804ed6d8383
somename <someemail>
2015-10-01 12:23:24 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -5,9 +5,13 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
+	String correctString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	String incorrectString ="123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 
 	@Test
-	public void test() {
+	public void test81CharactersLong() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		int test = sudoku.verify(correctString);
 		fail("Not yet implemented");
 	}
 
