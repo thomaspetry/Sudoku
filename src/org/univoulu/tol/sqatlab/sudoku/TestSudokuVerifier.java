abfb48212773c4cdd4dca9920f78581d1989a39e
somename <someemail>
2015-10-01 12:44:31 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -33,7 +33,7 @@ public class TestSudokuVerifier {
 	@Test
 	public void testRule1ResultsMinus1() {
 		int test = sudoku.rule1(notDigitsString);
-		assertEquals(test, test=-1);
+		assertEquals(test, test=0);
 	}
 
 }
